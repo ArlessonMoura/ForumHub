66a9e5491016b47ef1359f43f51c1076b506020c
ArlessonMoura <arlessonmss@gmail.com>
2024-07-16 22:56:09 -0300

    Security Layer (Me + sonnet IA)

--- src/main/java/br/com/oraclechallenge/ForumHub/service/security/AuthenticationService.java
+++ src/main/java/br/com/oraclechallenge/ForumHub/service/security/AuthenticationService.java
@@ -1,4 +1,18 @@
 package br.com.oraclechallenge.ForumHub.service.security;
 
-public class AuthenticationService {
+import br.com.araujo.jonas.ForumHub.repository.UsuarioDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    private UsuarioDetailsRepository userDetailsRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userDetailsRepository.findByLogin(username);
+    }
 }
